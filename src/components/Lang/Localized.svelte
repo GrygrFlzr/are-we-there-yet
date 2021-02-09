83ca62c627974e409549ffcd83a48f3f917e6c03
GrygrFlzr <grygrflzr@hotmail.com>
2021-02-09 16:23:25 +0700

    Simplify Localized replacement logic

--- src/components/Lang/Localized.svelte
+++ src/components/Lang/Localized.svelte
@@ -20,23 +20,22 @@
     $: localize = localization.localize;
     $: {
         let htmlString = localize(key, fallbackKey);
-        if (items[0]) {
-            htmlString = htmlString.replaceAll('{0}', items[0].innerHTML);
-        }
-        if (items[1]) {
-            htmlString = htmlString.replaceAll('{1}', items[1].innerHTML);
-        }
-        if (items[2]) {
-            htmlString = htmlString.replaceAll('{2}', items[2].innerHTML);
+        for (const key in items) {
+            if (items[key]) {
+                htmlString = htmlString.replaceAll(
+                    `{${key}}`,
+                    items[key].innerHTML
+                );
+            }
         }
         innerHTML = htmlString;
     }
 </script>
 
 <div class="hidden">
+    <slot />
     <span bind:this={items[0]}><slot name="0" /></span>
     <span bind:this={items[1]}><slot name="1" /></span>
     <span bind:this={items[2]}><slot name="2" /></span>
-    <slot />
 </div>
 <div class={className}>{@html innerHTML}</div>
