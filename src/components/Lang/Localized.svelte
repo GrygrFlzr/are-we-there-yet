5591eb7568a71ed78db706ea46f2b105836260a7
GrygrFlzr <grygrflzr@hotmail.com>
2021-02-10 01:17:51 +0700

    Rework Localization system
    - Use real slots instead of innerHTML
    - Fixes tooltips clipping out of viewport
    - Simplified tooltip clamping logic with use:clamp directive

--- src/components/Lang/Localized.svelte
+++ src/components/Lang/Localized.svelte
@@ -4,38 +4,39 @@
 
     const lang = getContext('lang');
 
+    // look ahead for `{`
+    // or look behind for `}`
+    const splitRegex = /(?={)|(?<=})/g;
+
     let key = '';
     let fallbackKey = undefined;
     let className = '';
     export { key, fallbackKey, className as class };
 
-    let innerHTML = '';
-
     /**
-     * @type {HTMLElement[]}
+     * @param {string} text
      */
-    let items = [];
+    function splitString(text) {
+        return text.split(splitRegex);
+    }
 
     $: localization = createLocalization($lang);
     $: localize = localization.localize;
-    $: {
-        let htmlString = localize(key, fallbackKey);
-        for (const key in items) {
-            if (items[key]) {
-                htmlString = htmlString.replaceAll(
-                    `{${key}}`,
-                    items[key].innerHTML
-                );
-            }
-        }
-        innerHTML = htmlString;
-    }
+    $: text = localize(key, fallbackKey);
+    $: pieces = splitString(text);
 </script>
 
-<div class="hidden">
-    <slot />
-    <span bind:this={items[0]}><slot name="0" /></span>
-    <span bind:this={items[1]}><slot name="1" /></span>
-    <span bind:this={items[2]}><slot name="2" /></span>
+<div class={className}>
+    {#each pieces as piece (piece)}
+        {#if piece === '{0}'}
+            <slot name="0" />
+        {:else if piece === '{1}'}
+            <slot name="1" />
+        {:else if piece === '{2}'}
+            <slot name="2" />
+        {:else}
+            {piece}
+        {/if}
+    {/each}
 </div>
-<div class={className}>{@html innerHTML}</div>
+{#if false}<slot />{/if}
