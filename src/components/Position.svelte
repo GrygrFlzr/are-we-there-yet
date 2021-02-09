2692e067df0686c56c70e8c9190625952c8faf9e
GrygrFlzr <grygrflzr@hotmail.com>
2021-02-10 01:17:51 +0700

    Rework Localization system
    - Use real slots instead of innerHTML
    - Fixes tooltips clipping out of viewport
    - Simplified tooltip clamping logic with use:clamp directive

--- src/components/Position.svelte
+++ src/components/Position.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { onMount } from 'svelte';
-
     const positionColors = {
         TL: 'bg-red-100 text-red-800 border-red-200',
         TLC: 'bg-yellow-100 text-yellow-800 border-yellow-200',
@@ -13,53 +11,42 @@
 
     export let position;
 
-    let innerWidth;
-    let tooltip;
-    let customTranslate = '';
-    let rect;
-    let shouldMoveLeft = false;
-    let shouldMoveRight = false;
+    /**
+     * @param {HTMLElement} node
+     */
+    function clamp(node) {
+        // reset baseline
+        node.style.setProperty('--tw-translate-x', '-50%');
+        const { scrollWidth } = document.body;
+        const { left, right } = node.getBoundingClientRect();
 
-    onMount(() => {
-        innerWidth = document.body.scrollWidth;
-    });
-
-    function recalculate() {
-        rect = tooltip ? tooltip.getBoundingClientRect() : undefined;
+        if (left < 16) {
+            node.style.setProperty(
+                '--tw-translate-x',
+                `calc(-50% + ${16 - left}px)`
+            );
+        } else if (right > scrollWidth - 16) {
+            node.style.setProperty(
+                '--tw-translate-x',
+                `calc(-50% - ${right - (scrollWidth - 16)}px)`
+            );
+        }
     }
 
     $: colors =
         positionColors[position.acronym] ||
         'bg-gray-100 text-gray-800 border-gray-200';
-
-    // prevent tooltip from escaping widget
-    $: tooltip, recalculate();
-    $: tooltipRightEdge = rect ? rect.width + rect.left : 0;
-    $: shouldMoveLeft =
-        shouldMoveLeft ||
-        (rect && innerWidth ? tooltipRightEdge >= innerWidth - 4 : false);
-    $: tooltipLeftEdge = rect ? rect.left : 5;
-    $: shouldMoveRight =
-        shouldMoveRight || (rect ? tooltipLeftEdge <= 4 : false);
-    $: if (shouldMoveLeft) {
-        customTranslate = '--tw-translate-x: -85%;';
-    } else if (shouldMoveRight) {
-        customTranslate = '--tw-translate-x: -15%;';
-    } else {
-        customTranslate = '';
-    }
 </script>
 
-<span
+<div
     class="cursor-default group relative rounded border px-1 inline-block align-middle {colors}"
 >
     <span>{position.acronym}</span>
     <!-- Tooltip -->
-    <span
-        bind:this={tooltip}
-        class="z-50 rounded-sm px-2 py-1 pointer-events-none opacity-0 group-hover:opacity-100 focus:opacity-100 transition-opacity shadow border text-xs absolute whitespace-nowrap left-1/2 transform bottom-6 text-center -translate-x-1/2 {colors}"
-        style={customTranslate}
+    <div
+        class="block z-50 rounded-sm px-2 py-1 pointer-events-none opacity-0 group-hover:opacity-100 focus:opacity-100 transition-opacity shadow border text-xs absolute whitespace-nowrap left-1/2 transform bottom-6 text-center -translate-x-1/2 {colors}"
+        use:clamp
     >
         {position.name}
-    </span>
-</span>
+    </div>
+</div>
