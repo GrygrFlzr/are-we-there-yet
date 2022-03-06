967ffea66d435225c735d4230ebb23c3633f025f
Miles Baskin <milburbas@gmail.com>
2022-03-06 17:43:57 -0500

    Request -> Event

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,12 +2,12 @@ import { dev } from '$app/env';
 import type { GetSession, Handle } from '@sveltejs/kit';
 import type { GroupOrError } from '$lib/utils';
 
-export const handle: Handle<GroupOrError> = async ({ request, resolve }) => {
+export const handle: Handle<GroupOrError> = async ({ event, resolve }) => {
 	if (dev) {
 		const { default: dotenv } = await import('dotenv');
 		dotenv.config();
 	}
-	return await resolve(request);
+	return await resolve(event);
 };
 
 export const getSession: GetSession<GroupOrError, GroupOrError> = ({ locals }) => {
